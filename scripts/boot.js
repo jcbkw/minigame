4f9f37aeff9fac928b5416f34e805f39fe8000b3
jcbkw <diiannasinclar@aol.com>
2017-04-05 10:46:44 -0400

    Add player move functionality. Rename app initializing declare.js to boot.js.

---
+++ scripts/boot.js
@@ -0,0 +1,7 @@
+/**
+ * This is the first JavaScript code executed by this application.
+ * It creates the only global variable that this app intends to use.
+ * 
+ * @type Object
+ */
+var app = {};
--- scripts/player.js
+++ scripts/player.js
@@ -1,17 +1,50 @@
 app.player = {
 
     /**
+     * The player element
+     * @type Element
+     */
+    element: null,
+
+    /**
+     * The player width
+     * @type Number
+     */
+    width: 25,
+
+    /**
+     * The player height
+     * @type Number
+     */
+    height: 25,
+
+    /**
+     * Horizontal position of the player on the stage.
+     * @type Number  
+     */
+    x: 10, 
+    
+    /**
+     * Vertical position of the player on the stage.  
+     * @type Number
+     */
+    y: 20, 
+
+    /**
      * Draws the game player
      * @param {Element} parentElement 
      */
     render: function (parentElement) {
-
+        
         var playerElement = document.createElement('div');
 
         playerElement.classList.add('player');
         
-        playerElement.style.width = "25px";
-        playerElement.style.height = "25px";
+        playerElement.style.width = this.width + "px";
+        playerElement.style.height = this.height + "px";
+        
+        playerElement.style.top = this.y + "px";
+        playerElement.style.left = this.x + "px";
 
         parentElement.appendChild(playerElement);
 
