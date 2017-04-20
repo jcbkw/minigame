7a1358fbdcb04e35840ea2b09e52634ff8a0884b
jcbkw <diiannasinclar@aol.com>
2017-04-20 16:43:20 -0400

    Allow DisplayClip .cross/.crossTo to take floats as x/y points

--- scripts/tk/arrays.js
+++ scripts/tk/arrays.js
@@ -442,7 +442,7 @@
          * @example
          * arrays.range(0,10);          // [0,1,2,3,4,5,6,7,8,9,10]
          * arrays.range(-100,100,20);   // [-100,-80,-60,-40,-20,0,20,40,60,80,100]
-         * arrays.range('A','F');       // ['A','B','C','D','E','F')
+         * arrays.range('A','F');       // ['A','B','C','D','E','F']
          * arrays.range('m','r');       // ['m','n','o','p','q','r']
          */
         range: function (start, end, step) {
